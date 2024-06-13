96ea5e7707ee7e826aa86c3958c12b672d74db51
abhay0933 <abhay80912@gmail.com>
2024-06-13 15:29:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import RecipeList from './components/RecipeList'
-// import Home from './components/Home'
-import ImageCarousel from './components/Home'
 import Recepie from './components/Recepie'
+import Home from './components/Home'
+import RecipeList from './components/RecipeList'
 import { Route,RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 function App() {
 
-  // const route = createBrowserRouter([
-  //   {path: "/",
-  //    element: <Home />
-  // },
-  // {
-  //   path: '/foods',
-  //   element: <RecipeList />
-  // },
-  // {
-  //   path: "/recipes/:recipeId",
-  //   element: <Recepie />
-  // }
-  // ])
+  const route = createBrowserRouter([
+    {path: "/",
+     element: <Home />
+  },
+  {
+    path: '/foods',
+    element: <RecipeList />
+  },
+  {
+    path: "/recipes/:recipeId",
+    element: <Recepie />
+  }
+  ])
 
   return (
     <>
-     {/* <RouterProvider router={route} /> */}
-     <ImageCarousel/ >
+     <RouterProvider router={route} />
     </>
   )
 }
